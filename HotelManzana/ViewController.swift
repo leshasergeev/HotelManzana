05f2dcd7909adfd747934cd313624a9d1e6f0455
iBamboola <41113234+iBamboola@users.noreply.github.com>
2021-04-23 13:59:37 +0300

    Initial Commit

---
+++ HotelManzana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotelManzana
+//
+//  Created by Алексей Сергеев on 23.04.2021.
+//  Copyright © 2021 Алексей Сергеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
